2b93586e398753329632804638f5ff7a39967823
KingDee241 <57509042+KingDee241@users.noreply.github.com>
2024-08-16 16:46:51 -0400

    fix spacePhysics implementation

--- Assets/Scripts/SpacePhysics.cs
+++ Assets/Scripts/SpacePhysics.cs
@@ -5,20 +5,25 @@ using UnityEngine;
 public class SpacePhysics : MonoBehaviour
 {
     // GLOBAL PHYSICS CONSTANTS
-    const float UNIVERSAL_GRAVITY_CONSTANT = 1;
+    const float UNIVERSAL_GRAVITY_CONSTANT = 1f;
 
     // PER OBJECT CONFIGS
-    [SerializeField]
-    public bool emitsGravity { get; private set; }
-    public bool canMove { get; private set; }
-    [SerializeField]
-    public float mass { get; private set; }
+    
+    public bool emitsGravity;
+    public bool canMove;
+    [Range(0.0f, 10.0f)]
+    public float mass;
 
     // Internal Components
     Rigidbody2D rb;
 
     // Other things for internal physics logic
-    List<SpacePhysics> otherBodies;
+    List<SpacePhysics> otherBodies = new();
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();   
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -32,11 +37,14 @@ public class SpacePhysics : MonoBehaviour
     /// </summary>
     void DoPhysics()
     {
-        Vector3 netForce = Vector3.zero;
+        Vector2 netForce = Vector2.zero;
         foreach(SpacePhysics sp in otherBodies)
         {
-            netForce += GetForce(sp);
+            netForce += sp.GetForce(this);
+
+            Debug.Log(netForce);
         }
+        Debug.Log(netForce);
         rb.AddForce(netForce);
     }
 
@@ -45,17 +53,20 @@ public class SpacePhysics : MonoBehaviour
     /// Gets the force exerted by this object onto the target gameObject using Universal Gravity Formula. REQUIRES target has SpacePhysics component.
     /// </summary>
     /// <param name="target"></param>
-    Vector3 GetForce(SpacePhysics target)
+    Vector2 GetForce(SpacePhysics target)
     {
-        if (!emitsGravity) return Vector3.zero;
+        if (!emitsGravity || target == this) return Vector2.zero;
 
         // get all the values for the formula
-        float r = (transform.position - target.transform.position).magnitude;
-        Vector3 rHat = (transform.position - target.transform.position).normalized;
+        float r = (target.transform.position - transform.position).magnitude;
+        Vector2 rHat = (transform.position - target.transform.position).normalized;
         float m1 = this.mass;
         float m2 = target.mass;
+        if (m1 == 0) Debug.LogWarning(gameObject + " physics mass is 0.");
+        if (m2 == 0) Debug.LogWarning(target.gameObject + " physics mass is 0.");
         // use the formula
         float vecMag = UNIVERSAL_GRAVITY_CONSTANT * m1 * m2 / Mathf.Pow(r, 2);
+        Debug.Log(rHat.ToString() + " vmg " + vecMag);
         return rHat * vecMag;
 
     }
