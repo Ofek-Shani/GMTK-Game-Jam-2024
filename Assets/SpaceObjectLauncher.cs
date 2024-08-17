a754a97bc770123f89db1352d30975b40d2658f6
grahameron <cschemm@umich.edu>
2024-08-17 15:59:31 -0400

    Revert "Merge pull request #3 from Ofek-Shani/fix-trajectory-preview"
    This reverts commit ef370c76e855902a706ef87448cee7e64494292f, reversing
    changes made to 1b5c9f01d7c8b1495a9952fe0c3efebd4fd34534.

--- Assets/SpaceObjectLauncher.cs
+++ Assets/SpaceObjectLauncher.cs
@@ -8,24 +8,15 @@ public class SpaceObjectLauncher : MonoBehaviour
     const int SIM_NUM_STEPS = 100;
     const float SIM_STEP_SIZE = 1f;
 
-    public float mouseCooldownTimer = 1f;
-    float mouseCooldownCountdown = 0f;
-
-    
-    public float previewLifetime = 1;
-    float previewTimer = 0;
-
     //component references
     LineRenderer lineRenderer;
     Camera cam;
     GameManager manager;
 
     public GameObject objectToLaunch;
-    public GameObject trajectoryPreviewObject;
     GameObject toLaunchInstance;
-    GameObject previewInstance;
 
-    public float launchSpeed = 10;
+    public float LAUNCH_SPEED = 2;
     Vector2 launchVector = Vector2.zero;
 
     private void Awake()
@@ -39,27 +30,12 @@ public class SpaceObjectLauncher : MonoBehaviour
         manager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
     }
 
-    private void Update()
-    {
-        if ((Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0) && mouseCooldownCountdown <=0)
-        {
-            previewTimer = 0;
-            mouseCooldownCountdown = mouseCooldownTimer;
-            Destroy(previewInstance);
-        }
-        mouseCooldownCountdown -= Time.deltaTime;
-
-    }
-
 
     bool aiming = false;
     private void OnMouseDown()
     {
         if (manager.canLaunchersFire)
         {
-            previewTimer = 0;
-            launchVector = (cam.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
-
             aiming = true;
             toLaunchInstance = Instantiate(objectToLaunch, transform.position, new Quaternion());
             toLaunchInstance.GetComponent<SpacePhysics>().Pause();
@@ -83,27 +59,15 @@ public class SpaceObjectLauncher : MonoBehaviour
     {
         if (aiming)
         {
-            if (previewTimer <= 0)
-            {
-                previewInstance = Instantiate(trajectoryPreviewObject, transform.position, Quaternion.identity);
-                previewInstance.GetComponent<Rigidbody2D>().velocity = launchVector * launchSpeed;
-                Destroy(previewInstance, previewLifetime);
-                previewTimer = previewLifetime;
-            }
-            else
-            {
-                previewTimer -= Time.deltaTime;
-            }
+            launchVector = cam.ScreenToWorldPoint(Input.mousePosition) - transform.position;
 
-            launchVector = (cam.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
-
-            // List<Vector2> positions = toLaunchInstance.GetComponent<SpacePhysics>().Simulate(launchVector.normalized * LAUNCH_SPEED, SIM_STEP_SIZE, SIM_NUM_STEPS);
+            List<Vector2> positions = toLaunchInstance.GetComponent<SpacePhysics>().Simulate(launchVector.normalized * LAUNCH_SPEED, SIM_STEP_SIZE, SIM_NUM_STEPS);
             // Debug.Log(positions.Count);
-            //lineRenderer.positionCount = positions.Count;
-            //for (int i = 0; i < positions.Count; i++)
-            //{
-            //    lineRenderer.SetPosition(i, positions[i]);
-            //}
+            lineRenderer.positionCount = positions.Count;
+            for (int i = 0; i < positions.Count; i++)
+            {
+                lineRenderer.SetPosition(i, positions[i]);
+            }
         }
     }
 
@@ -130,9 +94,8 @@ public class SpaceObjectLauncher : MonoBehaviour
 
     void Launch()
     {
-        Destroy(previewInstance);
         toLaunchInstance.GetComponent<SpacePhysics>().Unpause();
-        toLaunchInstance.GetComponent<Rigidbody2D>().velocity = launchVector * launchSpeed;
+        toLaunchInstance.GetComponent<Rigidbody2D>().velocity = launchVector * LAUNCH_SPEED;
         toLaunchInstance.GetComponent<CircleCollider2D>().enabled = true;
         toLaunchInstance = null;
         aiming = false;
